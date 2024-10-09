7ee437702865fe09a295278fb7ed64085acd74d6
Arthur-Sto <124912423+Arthur-Sto@users.noreply.github.com>
2024-10-09 13:34:07 -0300

    Update ongrep.route.js

--- src/routes/ongrep.route.js
+++ src/routes/ongrep.route.js
@@ -2,7 +2,7 @@
 import { Router } from "express";
 import { authMiddleware, createMiddleware } from "../middlewares/global.middleware.js";
 
-import {ONGrepLogin,createONGrep,findONGrepById,updateONGrep }  from "../controllers/ONGrep.controller.js";
+import {ONGrepLogin,createONGrep,findONGrepById,updateONGrep }  from "../controllers/ongrep.controller.js";
 
 const ONGrepRouter = Router()
 
